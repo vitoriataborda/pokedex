5351957a8479e0d97a53bcc206e7b2f5e22a5402
vitoriataborda <vitorianahitab@gmail.com>
2022-05-19 11:07:06 +0000

    pokedex

---
+++ src/app/services.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  private url="https://pokeapi.co/api/v2/
+  "
+
+  constructor() { }
+}
