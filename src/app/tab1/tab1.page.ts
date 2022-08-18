ac5a74ae9ee6e9c16ea47963511baa210ce21419
vitoriataborda <vitorianahitab@gmail.com>
2022-08-18 14:40:49 -0300

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -30,6 +30,7 @@ export class Tab1Page implements OnInit {
         .buscarUmPokemon(pokemon.url)
         .subscribe((dadosPokemon) => {
           this.listaPokemon.push(dadosPokemon);
+          
 
         });
 
